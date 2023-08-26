e945232cc248c35594da393e107327b96260ea5f
Oscar Alberto Pina Pena <isjadeclo@gmail.com>
2023-08-25 22:39:22 -0400

    Deleting sensible data from repo

---
+++ api/database/migrations/2023_08_04_043820_modify_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Hash;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function(Blueprint $table){
+            $table->renameColumn('email','username');
+        });
+
+        /* 
+            CREATING THE ADMIN USER
+            Change the word inside HASH to set the corresponding password of the user.
+        */
+        DB::table('users')->insert([
+            ['name' => 'root','username' => 'root', 'password' => Hash::make('root')],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
